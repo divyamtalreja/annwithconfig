a01135a832d9e32308e6821f3e427e7a3083ede5
divyamtalreja <divyamtalreja16@gmail.com>
2022-01-23 13:48:41 +0530

    new commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+## edit below variables as per your requirements -
+REPO_NAME = "annwithconfig"
+AUTHOR_USER_NAME = "divyamtalreja"
+SRC_REPO = "src"
+LIST_OF_REQUIREMENTS = [tensorflow,
+matplotlib,
+seaborn,
+numpy,
+pandas]
+
+
+setup(
+    name=SRC_REPO,
+    version="0.0.1",
+    author=AUTHOR_USER_NAME,
+    description="Ann with modular coding example project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    author_email="divyamtalreja16@gmail.com",
+    packages=[SRC_REPO],
+    license="MIT",
+    python_requires=">=3.6",
+    install_requires=LIST_OF_REQUIREMENTS
+)
